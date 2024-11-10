ff166917b7c346147f8c3a5720a8603f9f1c1832
Bri Kirchgessner <165532255+BriLeighk@users.noreply.github.com>
2024-11-09 21:28:31 -0600

    Update page.js

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -1,4 +1,4 @@
-
+"use client";
 import { useState } from 'react';
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
