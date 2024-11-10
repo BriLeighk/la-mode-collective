df4ba03290f9b247a5b334451d96f70889f5c2fe
Bri Kirchgessner <165532255+BriLeighk@users.noreply.github.com>
2024-11-10 11:06:58 -0600

    Update About.js

--- app/Components/About.js
+++ app/Components/About.js
@@ -7,7 +7,7 @@
         
         <div className="mx-auto px-6 lg:px-8 mt-24 mb-24">
           <div className="">
-            <h2 className="text-5xl font-semibold tracking-tight text-[#4d5d53] sm:text-4xl text-[#D0F0C0]">About Us</h2>
+            <h2 className="text-5xl font-semibold tracking-tight text-[#4d5d53] sm:text-4xl">About Us</h2>
             <p className="mt-8  max-w-7xl text-pretty text-md font-medium text-[#4d5d53] sm:text-md/8">
             Step into a world where your wardrobe comes to life—digitally and beautifully organized. At La Mode Collective, we bring the art of outfit creation to your fingertips, combining elegance and ease for a truly bespoke styling experience. From weather-based outfit suggestions to a swipeable lookbook and curated inspiration boards, we empower you to rediscover your wardrobe with refined style and charm.
             </p>
