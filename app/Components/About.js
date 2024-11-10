a56f7adab6becaba7a9bcad959690cccdc32b92a
BriLeighk <kirchgessner@wisc.edu>
2024-11-09 18:44:44 -0600

    Hero section of landing page + partial about section & header

---
+++ app/Components/About.js
@@ -0,0 +1,24 @@
+
+  
+  export default function About() {
+    return (
+      <div className="relative isolate overflow-hidden">
+        
+        
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-4xl text-[#D0F0C0]">About Us</h2>
+            <p className="mt-8 text-pretty text-md font-medium text-white sm:text-md/8">
+            Step into a world where your wardrobe comes to life—digitally and beautifully organized. At La Mode Collective, we bring the art of outfit creation to your fingertips, combining elegance and ease for a truly bespoke styling experience. From weather-based outfit suggestions to a swipeable lookbook and curated inspiration boards, we empower you to rediscover your wardrobe with refined style and charm.
+            </p>
+
+            <p className="mt-8 text-pretty text-md font-medium text-white sm:text-md/8">
+            Upload your cherished pieces into your personalized digital closet, categorizing items with care to craft your very own virtual wardrobe. Each day, find outfits handpicked from your collection to match the weather and your unique taste.
+            </p>
+          </div>
+          
+        </div>
+      </div>
+    )
+  }
+  
